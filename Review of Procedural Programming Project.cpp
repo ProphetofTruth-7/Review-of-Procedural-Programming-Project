8e72cea3590ce8520280454a939460f2d2bad685
Malakye Brown <prophetoftruth07@gmail.com>
2026-01-27 19:12:05 -0500

    Completition

--- Review of Procedural Programming Project.cpp
+++ Review of Procedural Programming Project.cpp
@@ -6,7 +6,8 @@ using namespace std;
 
 int userChoiceCheck(int& x);
 int victoryCheck(int x, int y);
-void scoreUpdate(int x);
+void scoreUpdate(int x, int& y, int& z);
+void declareVictory(int x, int y);
 
 
 int main()
@@ -15,19 +16,46 @@ int main()
     mt19937 gen(rd());
     uniform_int_distribution<>  dist(1, 3);   //RNG Establishment. Kept separate to prevent hyper-establishment
 
-    int roShamBoChoice = 0;
+    int roShamBoChoice = 0, userScore = 0, roShamBotScore = 0;
     cout << "Hello! Ready to Ro, Sham, and Bo?(1 for Rock, 2 for Paper, 3 for Scissors, 4 to end the game)\n";
 
     do {
         userChoiceCheck(roShamBoChoice);
         int botChoice = dist(gen);  //Random Number, Move to Function
         if (roShamBoChoice != 4) {
-            cout << victoryCheck(botChoice, roShamBoChoice) << endl;    //This couts the 0, 1, and 2 you're seeing, doofus. Don't forget
+            scoreUpdate(victoryCheck(botChoice, roShamBoChoice), roShamBotScore, userScore);
         }
     } while (roShamBoChoice != 4);
 
+    cout << endl;
     cout << "GAME OVER!" << endl;
+    declareVictory(userScore, roShamBotScore);
     return 0;
+}
+
+void declareVictory(int x, int y) {
+    cout << "User Score: " << x << "       RoShamBot Score: " << y << endl;
+    if (x > y) {
+        cout << "You have Won! Congrats!" << endl;
+    }
+    else if (x < y) {
+        cout << "RoShamBot has Won! Congrats?" << endl;
+    }
+    else {
+        cout << "There is a draw? Yay?" << endl;
+    }
+}
+
+void scoreUpdate(int x, int& y, int& z) {
+    if (x == 0) {
+        //Do nothing
+    }
+    else if (x == 1){
+        ++y;
+    }
+    else {
+        ++z;
+    }
 }
 
 int userChoiceCheck(int& x) {
@@ -42,6 +70,7 @@ int userChoiceCheck(int& x) {
 int victoryCheck(int x, int y) {  // Checks the User and the Bots choices, and returns 0 for Draw, 1 for Bot Win, and 2 for user win
     if (y == 4) {
         //Ends victoryCheck if you ended the game
+        return 3;
     }
     else {
         if (x == y) {
@@ -74,4 +103,5 @@ int victoryCheck(int x, int y) {  // Checks the User and the Bots choices, and r
         }
 
     }
+    return 3;
 }
