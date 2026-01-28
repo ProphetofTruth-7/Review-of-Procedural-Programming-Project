bea880c325ddffca211ee83d77de6470b31dd673
Malakye Brown <prophetoftruth07@gmail.com>
2026-01-27 20:22:35 -0500

    Added File and Getline(not sure how to use yet)

--- Review of Procedural Programming Project.cpp
+++ Review of Procedural Programming Project.cpp
@@ -1,10 +1,29 @@
 // Grade Book Branch
 
 #include <iostream>
+#include <fstream>
+#include <string>
+
 using namespace std;
 
 int main()
 {
+    string filename = "StudentGrades.txt";
+
+    ifstream inFile(filename);
+    if (!inFile) {
+            cout << "Error opening file!" << endl;
+        return 1;
+    }
+
+    string line;
+    while (getline(inFile, line)) {
+        cout << line << endl;
+    }
+
+
+
+
     cout << "This is the Grade Book Subproject\n";
     return 0;
 }
