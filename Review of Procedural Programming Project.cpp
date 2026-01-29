47f5ac4d7603808d67423240ee616e14291a9bae
Malakye Brown <prophetoftruth07@gmail.com>
2026-01-29 12:05:35 -0500

    Professor Help

--- Review of Procedural Programming Project.cpp
+++ Review of Procedural Programming Project.cpp
@@ -8,13 +8,20 @@ using namespace std;
 
 int main()
 {
-    string filename = "StudentGrades.txt";
-
+    string filename = "StudentGrades.txt"; //File Initialization bits
     ifstream inFile(filename);
     if (!inFile) {
             cout << "Error opening file!" << endl;
         return 1;
     }
+
+
+    const int TOTALSCORES = 5;
+
+    string testy;
+
+    getline(inFile, testy);   //How do I split the Name from the Test Scores? This reads the entire line no matter what
+    cout << testy << endl;
 
     return 0;
 }
