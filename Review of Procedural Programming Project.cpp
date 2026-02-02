67899fa751fe88999413c37c758c112e3abce25b
Malakye Brown <prophetoftruth07@gmail.com>
2026-02-02 10:06:24 -0500

    Checkpoint to look at other subproject

--- Review of Procedural Programming Project.cpp
+++ Review of Procedural Programming Project.cpp
@@ -3,14 +3,17 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
 using namespace std;
+
+string firstName
 
 int main()
 {
+    
+    
     string filename = "StudentGrades.txt"; //File Initialization bits
-    ifstream inFile(filename);
-    if (!inFile) {
+    ifstream GRADEFILE(filename);
+    if (!GRADEFILE) {
             cout << "Error opening file!" << endl;
         return 1;
     }
@@ -22,19 +25,21 @@ int main()
 
     string StudentNameArray[10]; //Partially Filled Array for Student Names. 10 is Arbitary
     int currentSize; // Used for the size of other Arrays. Depends on how filled the StudentNameArray is
-    int TestScoresArray[currentSize][TOTALSCORES]; //Filled Array. 5 Rows for 5 Students, TOTALSCORES Columns for number of TestScores
-    double AverageScoreArray[currentSize]; //Filled Array. Empty for now, 5 spots for 5 students.
+    int TestScoresArray[5][TOTALSCORES]; //Filled Array. 5 Rows for 5 Students, TOTALSCORES Columns for number of TestScores
+    double AverageScoreArray[5]; //Filled Array. Empty for now, 5 spots for 5 students.
 
 
 
-    getline(inFile, testy, ' '); //Ensure each value on attached file ends with a space. Failure to do so will cause errors
+    getline(GRADEFILE, testy, ' '); //Ensure each value on attached file ends with a space. Failure to do so will cause errors
     cout << testy << " ";
 
     do {
-        getline(inFile, testy, ' ');
+        getline(GRADEFILE, testy, ' ');
         cout << testy << " ";
         ++loopIncrement;
     } while (loopIncrement < (TOTALSCORES));
 
     return 0;
 }
+
+
