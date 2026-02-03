87771822e6d63e02cad60c972e1d587ceb779ef6
Malakye Brown <prophetoftruth07@gmail.com>
2026-02-03 09:52:20 -0500

    StudentNameArray Established

--- Review of Procedural Programming Project.cpp
+++ Review of Procedural Programming Project.cpp
@@ -9,24 +9,22 @@ string firstName(ifstream& x, string y) {
     getline(x, y, ' ');
     return y;
 }
-void storeNames(ifstream& x, string y, string array[], int z) {
-    int increment = 0, gradeIncrement = 0;
+void storeNames(ifstream& x, string y, string array[], int z, int& a) {
+    int nameIncrement = 0, gradeIncrement = 0;
 
     do {
         gradeIncrement = 0;
         getline(x, y, ' ');
-        array[increment++] = y;
+        array[nameIncrement++] = y;
         do {
             getline(x, y, ' ');
             gradeIncrement++;
         } while (gradeIncrement < z);
-    } while (increment < 6);         //Figure out how to make this variable
+    } while (nameIncrement < 10);         //Arbitary 10
 }
 
 int main()
 {
-    
-    
     string filename = "StudentGrades.txt"; //File Initialization bits
     ifstream GRADEFILE(filename);
     if (!GRADEFILE) {
@@ -36,18 +34,20 @@ int main()
 
 
     const int TOTALSCORES = 5;
-    string testy;
-    int loopIncrement = 0;
+    string fileIndexVar;
 
     string StudentNameArray[10]; //Partially Filled Array for Student Names. 10 is Arbitary
-    int currentSize; // Used for the size of other Arrays. Depends on how filled the StudentNameArray is
+    int currentSize = 0; // Used for the size of other Arrays. Depends on how filled the StudentNameArray is
     int TestScoresArray[5][TOTALSCORES]; //Filled Array. 5 Rows for 5 Students, TOTALSCORES Columns for number of TestScores
     double AverageScoreArray[5]; //Filled Array. Empty for now, 5 spots for 5 students.
 
-    storeNames(GRADEFILE, testy, StudentNameArray, TOTALSCORES);
+    storeNames(GRADEFILE, fileIndexVar, StudentNameArray, TOTALSCORES, currentSize);
     cout << StudentNameArray[0] << endl;
     cout << StudentNameArray[1] << endl;
     cout << StudentNameArray[5] << endl;
+    cout << StudentNameArray[9] << endl;
+    cout << "Current Size: " << currentSize << endl;
+
 
     return 0;
 }
