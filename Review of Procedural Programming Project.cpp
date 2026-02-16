d41e5a8082ba09530d129bf4a04c02a9fb278984
Malakye Brown <prophetoftruth07@gmail.com>
2026-02-16 11:02:53 -0500

    commit

--- Review of Procedural Programming Project.cpp
+++ Review of Procedural Programming Project.cpp
@@ -9,7 +9,7 @@ const int TOTAL_SCORES = 5;
 const int MAX_STUDENTS = 35;
 
 
-int studentDataRetrieval(ifstream& x, string student[], string scores[MAX_STUDENTS][TOTAL_SCORES], string y) {
+int studentDataRetrieval(ifstream& x, string student[], string scores[MAX_STUDENTS][TOTAL_SCORES], string y, double k) {
     int counter = 0, gradeIncrement = 0;
 
     while (counter < MAX_STUDENTS && x >> student[counter]) {
@@ -17,13 +17,19 @@ int studentDataRetrieval(ifstream& x, string student[], string scores[MAX_STUDEN
         getline(x, y, ' ');
         student[counter] = y;    // It's no longer storing the names!!!
         do {
-            getline(x, y, ' ');
-            scores[counter][gradeIncrement] = y;      //The scores Array (aka the TestScores Array) refuses to be assigned unless its a string
+            x >> k;
+            scores[counter][gradeIncrement] = k;      //The scores Array (aka the TestScores Array) refuses to be assigned unless its a string
             gradeIncrement++;
         } while (gradeIncrement < TOTAL_SCORES);
         ++counter;
     }
     return counter;
+}
+void testFunction(ifstream& x, string scores[MAX_STUDENTS][TOTAL_SCORES], int& y) {
+    x >> y;
+    x >> y;
+    x >> y;
+    cout << y;
 }
 
 
@@ -42,8 +48,9 @@ int main()
     double AverageScoreArray[MAX_STUDENTS]; //Filled Array. Empty for now, 5 spots for 5 students.
 
     string fileIndexVar;
+    double testy = 0;
 
-    cout << "The Current Size is: " << studentDataRetrieval(GRADEFILE, StudentNameArray, TestScoresArray, fileIndexVar) << endl;
+    cout << "The Current Size is: " << studentDataRetrieval(GRADEFILE, StudentNameArray, TestScoresArray, fileIndexVar, testy) << endl;
 
     cout << "The First Student is: " << StudentNameArray[0] << endl;
     cout << TestScoresArray[0][0] << " " << TestScoresArray[0][1] << " " << " " << TestScoresArray[0][2] << " " << " " << TestScoresArray[0][3] << " " << " " << TestScoresArray[0][4] << endl;
